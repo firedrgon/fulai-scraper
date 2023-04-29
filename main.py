0640b2da00048ef45d8b47951f29e08b1b97e7a3
firedrgon <firedrgon@users.noreply.github.com>
2023-04-29 22:20:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ logger.addHandler(logger_file_handler)
 
 try:
     token = os.environ["TOKEN"]
+    token02 = os.environ["TOKEN_02"]
 except KeyError:
     logger.info("Token not available!")
 
@@ -31,6 +32,23 @@ if __name__ == "__main__":
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh',
         'token': token
+    }
+    r = requests.get('https://api.fulaizhitou.com/tools/task/sign', headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        code = data["code"]
+        dou = data["dou"]
+        logger.info(f'code: {code}, dou: {dou}')
+        
+    headers = {
+        'Host': 'api.fulaizhitou.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) '
+                      'WindowsWechat(0x63090217) XWEB/6763 Flue',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh',
+        'token': token02
     }
     r = requests.get('https://api.fulaizhitou.com/tools/task/sign', headers=headers)
     if r.status_code == 200:
