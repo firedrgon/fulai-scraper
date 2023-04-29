a50b1f9f7c5e766453c1e38c5526bf5a7406357a
firedrgon <775354391@qq.com>
2023-04-29 22:34:49 +0800

    cron modify

--- main.py
+++ main.py
@@ -40,16 +40,7 @@ if __name__ == "__main__":
         dou = data["dou"]
         logger.info(f'code: {code}, dou: {dou}')
         
-    headers = {
-        'Host': 'api.fulaizhitou.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) '
-                      'WindowsWechat(0x63090217) XWEB/6763 Flue',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh',
-        'token': token02
-    }
+    headers['token'] = token02
     r = requests.get('https://api.fulaizhitou.com/tools/task/sign', headers=headers)
     if r.status_code == 200:
         data = r.json()
